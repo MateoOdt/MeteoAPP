750a855aebf8dc023a7723f35c3a34e25c0e2841
Lucas MESSIA <lmessia93@gmail.com>
2024-01-30 21:35:47 +0100

    component+ screen Geo
    chore: Mise en place de la fonctionnalité météo avec gestion des unités
    
    - Ajout d'un bouton dans WeatherComponent pour basculer entre Celsius et Fahrenheit.
    - Mise à jour de l'URL de l'API pour inclure l'unité de température sélectionnée (metric ou imperial).
    - Affichage de la température avec l'unité correspondante (°C ou °F) en fonction du bouton de bascule.
    
    feat: Ajout de la gestion des erreurs pour les appels API dans WeatherComponent
    
    - Ajout de gestionnaires d'erreurs pour l'appel à l'API de localisation et à l'API météo.
    - Affichage des messages d'erreur dans la console pour un débogage plus efficace.
    
    docs: Mise à jour des imports dans les fichiers de composants
    
    - Modification des imports pour utiliser des chemins relatifs.
    - Correction des erreurs potentielles dans les chemins d'importation.
    
    Fixes #issueNumber (si applicable)

---
+++ src/screen/WeatherScreen.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import WeatherComponent from '../components/WeatherComponent';
+
+const WeatherScreen = () => {
+    return (
+        <WeatherComponent />
+    );
+};
+
+export default WeatherScreen;
