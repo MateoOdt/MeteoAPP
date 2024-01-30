efdcb67c1ace1e59ef0b32e88200ef5d2cb2c515
Oudart Matéo <mateooudart@gmail.com>
2024-01-30 14:56:45 +0100

    dev branch

--- screen/Home.screen.jsx
+++ screen/Home.screen.jsx
@@ -1,21 +1,7 @@
 import { Text } from "react-native";
-import { useState } from "react";
-import { getWeather } from "../service/weather/weatherService";
-import { useEffect } from "react";
 
 const HomeScreen = async () => {
-  const [weather, setWeather] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getWeather();
-      setWeather(data);
-    };
-    fetchData();
-  }, []);
-  console.log(weather);
-
-  return <Text>AAAAA</Text>;
+  return <Text>Welcome on Dev Branch</Text>;
 };
 
 export default HomeScreen;
