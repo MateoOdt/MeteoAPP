ad57ce58872b0393d9e77b4242ee2c2283044b09
Oudart Matéo <mateooudart@gmail.com>
2024-01-31 22:10:21 +0100

    autocomplete done

--- screen/Home.screen.jsx
+++ screen/Home.screen.jsx
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, ActivityIndicator } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { getWeatherByCity } from "../services/weather/weatherService";
-import { TextInput } from "react-native-paper";
+import { Menu, Button, Provider as PaperProvider } from "react-native-paper";
 import { getAddressesByQuery } from "../services/address/addressService";
+import { LinearGradient } from "expo-linear-gradient";
+import { TouchableOpacity } from "react-native";
+import { TextInput } from "react-native-paper";
 
 const HomeScreen = () => {
   const [weather, setWeather] = useState({});
   const [address, setAddress] = useState("");
   const [addressOptions, setAddressOptions] = useState(null);
+  const [visible, setVisible] = useState(false);
+  const [selectedAddress, setSelectedAddress] = useState("");
 
   useEffect(() => {
     if (address !== "") {
@@ -19,9 +24,10 @@ const HomeScreen = () => {
     }
   }, [address]);
 
-  useEffect(() => {
-    if (addressOptions !== null) {
-      getWeatherByCity(addressOptions[0]?.properties?.label)
+  /**
+   * useEffect(() => {
+    if (selectedAddress !== "") {
+      getWeatherByCity(selectedAddress)
         .then((data) => {
           setWeather(data);
         })
@@ -29,23 +35,74 @@ const HomeScreen = () => {
           console.error(error);
         });
     }
-  }, [addressOptions]);
+  }, [selectedAddress]);
+   */
 
   console.log(weather);
 
+  const handleSelectAddress = (addressLabel) => {
+    setAddress(addressLabel);
+    setSelectedAddress(addressLabel);
+    setVisible(false);
+  };
+
   return (
-    <View>
-      <Text>Search for city</Text>
+    <LinearGradient
+      colors={["rgba(64,31,79,1)", "rgba(61,41,115,1)"]}
+      style={styles.container}
+    >
+      <Text style={styles.title}>Search for city</Text>
       <TextInput
         label="Address"
+        mode="outlined"
         value={address}
-        onChangeText={(text) => setAddress(text)}
+        onChangeText={(text) => {
+          setAddress(text);
+          setVisible(true);
+        }}
+        style={styles.input}
       />
-      <Text>Wind Speed : {weather?.wind?.speed ?? "No info"}</Text>
-      <Text>Humidity : {weather?.main?.humidity ?? "No info"}</Text>
-      <Text>Weather : {weather?.weather[0]?.description ?? "No info"}</Text>
-    </View>
+      {addressOptions && visible !== false && (
+        <View style={styles.autocompleteContainer}>
+          {addressOptions.map((option, index) => (
+            <TouchableOpacity
+              key={index}
+              style={styles.option}
+              onPress={() => handleSelectAddress(option.properties.label)}
+            >
+              <Text style={styles.optionText}>{option.properties.label}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      )}
+    </LinearGradient>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 0,
+    flex: 1,
+    padding: 30,
+  },
+  title: {
+    marginTop: 50,
+    fontWeight: "600",
+    fontSize: 18,
+    textAlign: "center",
+    color: "#fff",
+  },
+  input: {
+    marginTop: 50,
+    width: 335,
+  },
+  autocompleteContainer: {
+    backgroundColor: "rgba(0,0,0,0.2)",
+    padding: 20,
+  },
+  optionText: {
+    color: "white",
+  },
+});
 
 export default HomeScreen;
