1099e47efcbe8d75a965c206d34cf3512975ed81
Lucas MESSIA <lmessia93@gmail.com>
2024-02-01 14:20:20 +0100

    Update WeatherComponent.js

--- src/components/WeatherComponent.js
+++ src/components/WeatherComponent.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Button, StyleSheet, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, Button, StyleSheet, Image, TouchableOpacity, ScrollView, ImageBackground } from 'react-native';
 import * as Location from 'expo-location';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
 
 const WeatherComponent = () => {
     const [location, setLocation] = useState(null);
@@ -134,6 +136,7 @@ const WeatherComponent = () => {
     }, []);
 
     return (
+        <ImageBackground source={require('../assets/Background.png')} style={styles.container}>
         <ScrollView contentContainerStyle={styles.container}>
             <Text style={styles.city}>{weather?.name}</Text>
             {loading ? (
@@ -166,6 +169,7 @@ const WeatherComponent = () => {
                 </View>
             )}
         </ScrollView>
+        </ImageBackground>
     );
 };
 
