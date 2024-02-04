619fb18750b41027ea73a5e63731260f3ae5c070
Oudart Matéo <mateooudart@gmail.com>
2024-02-04 17:03:48 +0100

    added feedback page on click on image weather

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { PaperProvider } from "react-native-paper";
 import WeatherScreen from "./src/screen/WeatherScreen";
 import HomeScreen from "./src/screen/Home.screen";
+import { FeedBack } from "./src/screen/Feedback.screen";
 const Stack = createStackNavigator();
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
         <Stack.Navigator initialRouteName="Home">
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Weather" component={WeatherScreen} />
+          <Stack.Screen name="Feedback" component={FeedBack} />
         </Stack.Navigator>
       </NavigationContainer>
     </PaperProvider>
